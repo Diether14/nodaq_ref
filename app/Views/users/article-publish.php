880ec951f2b42f354d458df20edddc72c6eafec3
michael <michaelolonan0075@gmail.com>
2020-04-24 23:55:30 +0800

    article interface

---
+++ app/Views/users/article-publish.php
@@ -0,0 +1,416 @@
+<style>
+.btn-circle {
+  width: 45px;
+  height: 45px;
+  line-height: 45px;
+  text-align: center;
+  padding: 0;
+  border-radius: 50%;
+}
+
+.btn-circle i {
+  position: relative;
+  top: -1px;
+}
+
+.btn-circle-sm {
+  width: 35px;
+  height: 35px;
+  line-height: 35px;
+  font-size: 0.9rem;
+}
+
+.btn-circle-lg {
+  width: 55px;
+  height: 55px;
+  line-height: 55px;
+  font-size: 1.1rem;
+}
+
+.btn-circle-xl {
+  width: 70px;
+  height: 70px;
+  line-height: 70px;
+  font-size: 1.3rem;
+}
+
+</style>
+
+ <!-- Page Content -->
+ <div class="container">
+
+<div class="row">
+
+  <!-- Post Content Column -->
+  <div class="col-lg-8">
+    <div class="d-flex mt-3">
+    <div class="">
+    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal">
+      Publish
+    </button>
+    </div>
+    <div class="pl-3">
+      <button type="button" class="btn btn-secondary">Remove</button>
+    </div>
+    <div class="pl-3">
+      <button type="button" class="btn btn-danger">Report</button>
+    </div>
+
+    <!-- The Modal -->
+    <div class="modal fade" id="myModal">
+      <div class="modal-dialog">
+        <div class="modal-content">
+
+          <!-- Modal Header -->
+          <div class="modal-header">
+            <h4 class="modal-title">Publish Post Title</h4>
+            <button type="button" class="close" data-dismiss="modal">&times;</button>
+          </div>
+
+          <!-- Modal body -->
+          <div class="modal-body">
+          <form>
+          <div class="form-group">
+            <label for="exampleFormControlSelect1">Number of hours</label>
+            <select class="form-control" id="exampleFormControlSelect1">
+              <option>1 hour</option>
+              <option>6 hours</option>
+              <option>24 hours</option>
+              <option>48 hours</option>
+              <option>7 days</option>
+            </select>
+          </div>
+
+          <div class="form-group">
+            <label for="exampleFormControlSelect1">Reason</label>
+            <select class="form-control" id="exampleFormControlSelect1">
+              <option>Reason 1</option>
+              <option>Reason 2</option>
+              <option>Reason 3</option>
+              <option>Reason 4</option>
+              <option>Reason 5</option>
+            </select>
+          </div>
+          </form>
+          </div>
+
+          <!-- Modal footer -->
+          <div class="modal-footer">
+            <button type="button" class="btn btn-primary" data-dismiss="modal">Apply</button>
+            <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+          </div>
+
+        </div>
+      </div>
+    </div>
+    </div>
+    <!-- Title -->
+    <h1 class="mt-4">Post Title</h1>
+
+    <!-- Author -->
+    <p class="lead">
+      by
+      <a href="#">Nickname</a>
+    </p>
+
+    <hr>
+
+    <!-- Date/Time -->
+    <p>Posted on January 1, 2019 at 12:00 PM</p>
+
+    <hr>
+
+    <!-- Preview Image -->
+    <img class="img-fluid rounded" src="http://placehold.it/900x300" alt="">
+
+    <hr>
+
+    <!-- Post Content -->
+    <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus, vero, obcaecati, aut, error quam sapiente nemo saepe quibusdam sit excepturi nam quia corporis eligendi eos magni recusandae laborum minus inventore?</p>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, tenetur natus doloremque laborum quos iste ipsum rerum obcaecati impedit odit illo dolorum ab tempora nihil dicta earum fugiat. Temporibus, voluptatibus.</p>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos, doloribus, dolorem iusto blanditiis unde eius illum consequuntur neque dicta incidunt ullam ea hic porro optio ratione repellat perspiciatis. Enim, iure!</p>
+
+    <blockquote class="blockquote">
+      <p class="mb-0">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
+      <footer class="blockquote-footer">Someone famous in
+        <cite title="Source Title">Source Title</cite>
+      </footer>
+    </blockquote>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error, nostrum, aliquid, animi, ut quas placeat totam sunt tempora commodi nihil ullam alias modi dicta saepe minima ab quo voluptatem obcaecati?</p>
+
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum, dolor quis. Sunt, ut, explicabo, aliquam tenetur ratione tempore quidem voluptates cupiditate voluptas illo saepe quaerat numquam recusandae? Qui, necessitatibus, est!</p>
+
+    <hr>
+    
+    <div class="col-12 mb-3 ">
+    <div class="row justify-content-center">
+        <div class="col-3">
+        <div class="bg-white pl-4 rounded shadow-sm h-100 text-center">
+            <button class="btn btn-danger btn-circle btn-circle-xl" style="background-color:#E74C3C;"><i class="fa fa-chevron-up"></i></button>
+            <p class="lead  mb-0 small"><b>UPVOTE</b></p>
+            <h1 class="m-0">1334</h1>
+            <button class="btn btn-danger btn-circle btn-circle-xl" style="background-color:#8E44AD;"><i class="fa fa-chevron-down"></i></button>
+            <p class="lead mb-0 small"><b>DEVOTE</b></p>                      
+        </div>
+        </div>
+
+        <div class="col-3">
+        <div class="bg-white pl-4 rounded shadow-sm h-100 text-center">
+            <button class="btn btn-danger btn-circle btn-circle-xl" style="background-color:#2980B9;"><i class="fa fa-chevron-up"></i></button>
+            <p class="lead mb-0 small"><b>UPVOTE</b></p>
+            <h1 class="m-0">200</h1>
+            <button class="btn btn-danger btn-circle btn-circle-xl" style="background-color:#2C3E50;"><i class="fa fa-chevron-down"></i></button>
+            <p class="lead mb-0 small"><b>DEVOTE</b></p>                      
+        </div>
+        </div>
+        <div class="col-3">
+        <div class="bg-white pl-4 rounded shadow-sm h-100 text-center">
+            <button class="btn btn-danger btn-circle btn-circle-xl" style="background-color:#1ABC9C;"><i class="fa fa-chevron-up"></i></button>
+            <p class="lead  mb-0 small"><b>UPVOTE</b></p>
+            <h1 class="m-0">30</h1>
+            <button class="btn btn-danger btn-circle btn-circle-xl" style="background-color:#2D7C6D;"><i class="fa fa-chevron-down"></i></button>
+            <p class="lead  mb-0 small"><b>DEVOTE</b></p>                      
+        </div>
+        </div> 
+        <div class="d-flex mt-3">
+      
+          <div class="">
+          <button type="button" class="btn btn-primary btn-xl" >
+            Publish
+          </button>
+          </div>
+          <div class="pl-3">
+            <button type="button" class="btn btn-secondary btn-xl">Remove</button>
+          </div>
+          <div class="pl-3">
+            <button type="button" class="btn btn-danger btn-xl">Report</button>
+          </div>
+          </div>
+
+      </div>
+    
+    </div>
+    <div class="col-12 mb-5">
+    <div class="row justify-content-center">
+      <div class="card bg-light ">
+        <!-- <div class="card-header">Header</div> -->
+        <table class="table table-bordered">
+    <thead>
+      <tr>
+        <th>Nickname </th>
+        <th>IP two digits</th>
+        <th>Time</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>Nick1</td>
+        <td>127.0</td>
+        <td>4/24/2020</td>
+      </tr>
+      <tr>
+        <td>Nick1</td>
+        <td>127.0</td>
+        <td>4/24/2020</td>
+      </tr>
+      <tr>
+        <td>Nick1</td>
+        <td>127.0</td>
+        <td>4/24/2020</td>
+      </tr>
+    </tbody>
+   </table>
+   <div class="pl-2 pb-3 text-center">
+            <button type="button" class="btn btn-primary btn-xl">Collect</button>
+          </div>
+      </div>
+      </div>
+      </div>
+   
+        
+    <!-- Comments Form -->
+    <div class="card my-4">
+      <h5 class="card-header">Leave a Comment:</h5>
+      <div class="card-body">
+        <form>
+          <div class="form-group">
+            <textarea class="form-control" rows="3"></textarea>
+          </div>
+          <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
+      </div>
+    </div>
+
+    <!-- Single Comment -->
+    <div class="media mb-4">
+      <img class="d-flex mr-3 rounded-circle" src="http://placehold.it/50x50" alt="">
+      <div class="media-body">
+        <h5 class="mt-0">Commenter Name</h5>
+        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+      </div>
+    </div>
+
+    <!-- Comment with nested comments -->
+    <div class="media mb-4">
+      <img class="d-flex mr-3 rounded-circle" src="http://placehold.it/50x50" alt="">
+      <div class="media-body">
+        <h5 class="mt-0">Commenter Name</h5>
+        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+
+        <div class="media mt-4">
+          <img class="d-flex mr-3 rounded-circle" src="http://placehold.it/50x50" alt="">
+          <div class="media-body">
+            <h5 class="mt-0">Commenter Name</h5>
+            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+          </div>
+        </div>
+
+        <div class="media mt-4">
+          <img class="d-flex mr-3 rounded-circle" src="http://placehold.it/50x50" alt="">
+          <div class="media-body">
+            <h5 class="mt-0">Commenter Name</h5>
+            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+  </div>
+
+  <!-- Sidebar Widgets Column -->
+  <div class="col-md-4 mt-3">
+
+    <!-- Search Widget -->
+    <!-- <div class="card my-4">
+      <h5 class="card-header">Search</h5>
+      <div class="card-body">
+        <div class="input-group">
+          <input type="text" class="form-control" placeholder="Search for...">
+          <span class="input-group-btn">
+            <button class="btn btn-secondary" type="button">Go!</button>
+          </span>
+        </div>
+      </div>
+    </div> -->
+
+    <!-- Categories Widget -->
+    <div class="card my-4 ">
+      <h5 class="card-header">General</h5>
+      <div class="card-body">
+        <div class="row">
+          <div class="col-lg-6">
+            <ul class="list-unstyled mb-0">
+              <li>
+                <a href="#">Web Design</a>
+              </li>
+              <li>
+                <a href="#">HTML</a>
+              </li>
+              <li>
+                <a href="#">Freebies</a>
+              </li>
+            </ul>
+          </div>
+          <div class="col-lg-6">
+            <ul class="list-unstyled mb-0">
+              <li>
+                <a href="#">JavaScript</a>
+              </li>
+              <li>
+                <a href="#">CSS</a>
+              </li>
+              <li>
+                <a href="#">Tutorials</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="card my-4">
+      <h5 class="card-header">History</h5>
+      <div class="card-body">
+        <div class="row">
+          <div class="col-lg-6">
+            <ul class="list-unstyled mb-0">
+              <li>
+                <a href="#">Web Design</a>
+              </li>
+              <li>
+                <a href="#">HTML</a>
+              </li>
+              <li>
+                <a href="#">Freebies</a>
+              </li>
+            </ul>
+          </div>
+          <div class="col-lg-6">
+            <ul class="list-unstyled mb-0">
+              <li>
+                <a href="#">JavaScript</a>
+              </li>
+              <li>
+                <a href="#">CSS</a>
+              </li>
+              <li>
+                <a href="#">Tutorials</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    <div class="card my-4">
+      <h5 class="card-header">Storage</h5>
+      <div class="card-body">
+        <div class="row">
+          <div class="col-lg-6">
+            <ul class="list-unstyled mb-0">
+              <li>
+                <a href="#">Web Design</a>
+              </li>
+              <li>
+                <a href="#">HTML</a>
+              </li>
+              <li>
+                <a href="#">Freebies</a>
+              </li>
+            </ul>
+          </div>
+          <div class="col-lg-6">
+            <ul class="list-unstyled mb-0">
+              <li>
+                <a href="#">JavaScript</a>
+              </li>
+              <li>
+                <a href="#">CSS</a>
+              </li>
+              <li>
+                <a href="#">Tutorials</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Side Widget -->
+    <div class="card my-4">
+      <h5 class="card-header">Side Widget</h5>
+      <div class="card-body">
+        You can put anything you want inside of these side widgets. They are easy to use, and feature the new Bootstrap 4 card containers!
+      </div>
+    </div>
+
+  </div>
+
+</div>
+<!-- /.row -->
+
+</div>
