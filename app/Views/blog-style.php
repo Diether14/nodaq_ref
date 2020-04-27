996b8661d8258ec190dd38db59b3fe1d154185fe
Michael <michaelolonan0075@gmail.com>
2020-04-28 00:07:32 +0800

    pages 5,6,7,8

---
+++ app/Views/blog-style.php
@@ -0,0 +1,154 @@
+<style>
+  .nav-link[data-toggle].collapsed:before {
+    content: " ▾";
+  }
+
+  .nav-link[data-toggle]:not(.collapsed):before {
+    content: " ▴";
+  }
+</style>
+<div class="container mb-5" style="margin-top:10px">
+
+  <div class="mt-2">
+    <button type="button" class="btn btn-danger btn-circle btn-xl mr-3"><i class="fa fa-arrow-left"></i></button>
+    <button class="btn btn-primary  mr-1">Report the rules of Wuhan Gallery</button>
+    <button class="btn btn-primary mr-1">Report Bulletin</button>
+    <button class="btn btn-primary ">Information ..</button>
+    <button type="button" class="btn btn-secondary btn-circle btn-xl mr-3"><i class="fa fa-plus"></i></button>
+  </div>
+  <hr />
+
+
+  <div class="row">
+    <div class="col-sm-3">
+
+      <div class="card text-center" style="width: 16rem;">
+        <img class="card-img-top"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQEaKLjyiVsq7Jch5AQBREAHsBZxhjFZa9HSWxtV-x8InEalpgV&usqp=CAU"
+          alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title">Nickname</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+            content.</p>
+          <p class="card-text">Guest Book</p>
+        </div>
+      </div>
+      <div class="card mt-4">
+        <ul class="nav flex-column flex-nowrap overflow-hidden">
+          <li class="nav-item">
+            <a class="nav-link collapsed text-truncate" href="#submenu1" data-toggle="collapse"
+              data-target="#submenu1"><i class=""></i> <span class="d-none d-sm-inline">General</span></a>
+            <div class="collapse" id="submenu1" aria-expanded="false">
+              <ul class="flex-column pl-2 nav">
+                <li class="nav-item"><a class="nav-link py-0" href="#"><span>subclass1</span></a></li>
+                <li class="nav-item"><a class="nav-link py-0" href="#"><span>subclass2</span></a></li>
+                <li class="nav-item"><a class="nav-link py-0" href="#"><span>subclass3</span></a></li>
+              </ul>
+            </div>
+            <a class="nav-link collapsed text-truncate" href="#submenu2" data-toggle="collapse"
+              data-target="#submenu2"><i class=""></i> <span class="d-none d-sm-inline">History</span></a>
+            <div class="collapse" id="submenu2" aria-expanded="false">
+              <ul class="flex-column pl-2 nav">
+                <li class="nav-item"><a class="nav-link py-0" href="#"><span>subclass1</span></a></li>
+                <li class="nav-item"><a class="nav-link py-0" href="#"><span>subclass2</span></a></li>
+                <li class="nav-item"><a class="nav-link py-0" href="#"><span>subclass3</span></a></li>
+              </ul>
+            </div>
+            <a class="nav-link collapsed text-truncate" href="#submenu3" data-toggle="collapse"
+              data-target="#submenu3"><i class=""></i> <span class="d-none d-sm-inline">Storage</span></a>
+            <div class="collapse" id="submenu3" aria-expanded="false">
+              <ul class="flex-column pl-2 nav">
+                <li class="nav-item"><a class="nav-link py-0" href="#"><span>subclass1</span></a></li>
+                <li class="nav-item"><a class="nav-link py-0" href="#"><span>subclass2</span></a></li>
+                <li class="nav-item"><a class="nav-link py-0" href="#"><span>subclass3</span></a></li>
+              </ul>
+            </div>
+          </li>
+
+        </ul>
+      </div>
+
+
+      <hr class="d-sm-none">
+    </div>
+    <div class="col-sm-9">
+      <div class="d-flex align-items-center">
+        <h4 class="mr-3">Hottest</h4>
+        <button class="btn btn-xs mr-1"><span class="fa fa-arrow-right"></span></button>
+        <button class="btn btn-xs mr-1"><span class="fa fa-book"></span></button>
+        <button class="btn btn-xs"><span class="fa fa-list"></span></button>
+
+      </div>
+
+      <div class="row mt-3">
+        <div class="col-sm-6">
+
+          <!-- Media middle -->
+          <div class="media">
+            <img src="https://dummyimage.com/vga" class="align-self-center mr-3" style="width:100px" height="100px">
+            <div class="media-body">
+              <h4>Media Middle</h4>
+              <p>Lorem ipsum...</p>
+            </div>
+          </div>
+          <!-- Media middle -->
+          <div class="media mt-2">
+            <img src="https://dummyimage.com/vga" class="align-self-center mr-3" style="width:100px" height="100px">
+            <div class="media-body">
+              <h4>Media Middle</h4>
+              <p>Lorem ipsum...</p>
+            </div>
+          </div>
+          <!-- Media middle -->
+          <div class="media mt-2">
+            <img src="https://dummyimage.com/vga" class="align-self-center mr-3" style="width:100px" height="100px">
+            <div class="media-body">
+              <h4>Media Middle</h4>
+              <p>Lorem ipsum...</p>
+            </div>
+          </div>
+
+        </div>
+        <div class="col-sm-6">
+
+          <!-- Media middle -->
+          <div class="media mt-2">
+            <img src="https://dummyimage.com/vga" class="align-self-center mr-3" style="width:100px" height="100px">
+            <div class="media-body">
+              <h4>Media Middle</h4>
+              <p>Lorem ipsum...</p>
+            </div>
+          </div>
+          <!-- Media middle -->
+          <div class="media mt-2">
+            <img src="https://dummyimage.com/vga" class="align-self-center mr-3" style="width:100px" height="100px">
+            <div class="media-body">
+              <h4>Media Middle</h4>
+              <p>Lorem ipsum...</p>
+            </div>
+          </div>
+          <!-- Media middle -->
+          <div class="media mt-2">
+            <img src="https://dummyimage.com/vga" class="align-self-center mr-3" style="width:100px" height="100px">
+            <div class="media-body">
+              <h4>Media Middle</h4>
+              <p>Lorem ipsum...</p>
+            </div>
+          </div>
+
+        </div>
+      </div>
+      <div class="col-lg-12 mt-3">
+        <div class="card mb-3">
+          <img class="card-img-top" src="https://dummyimage.com/vga" alt="Card image cap">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional
+              content. This content is a little bit longer.</p>
+            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
--- app/Views/cafe-style.php
+++ app/Views/cafe-style.php
@@ -1,228 +1,263 @@
+<style>
+  .nav-link[data-toggle].collapsed:before {
+    content: " ▾";
+  }
 
- <!-- Page Content -->
- <div class="container">
-        <div class="mt-3">
-        <button class="btn btn-primary  mr-3">Report the rules of Wuhan Gallery</button>
-        <button class="btn btn-primary mr-3">Report Bulletin</button>
-        <button class="btn btn-primary ">Information ..</button>
+  .nav-link[data-toggle]:not(.collapsed):before {
+    content: " ▴";
+  }
+</style>
+<!-- Page Content -->
+<div class="container mb-5">
+  <div class="mt-3">
+    <button type="button" class="btn btn-danger btn-circle btn-xl mr-3"><i class="fa fa-arrow-left"></i></button>
+    <button class="btn btn-primary  mr-1">Report the rules of Wuhan Gallery</button>
+    <button class="btn btn-primary mr-1">Report Bulletin</button>
+    <button class="btn btn-primary ">Information ..</button>
+    <button type="button" class="btn btn-secondary btn-circle btn-xl mr-3"><i class="fa fa-plus"></i></button>
+  </div>
+  <hr />
+  <div class="row">
+
+    <!-- Post Content Column -->
+    <div class="col-md-3">
+      <div class="text-center">
+        <!-- <div class="card"> -->
+        <button class="btn btn-primary btn-xl" style="width: 50%;">Join in</button>
+        <!-- </div> -->
+      </div>
+      <!-- Side Widget -->
+      <div class="card mt-4">
+        <ul class="nav flex-column flex-nowrap overflow-hidden">
+          <li class="nav-item">
+            <a class="nav-link collapsed text-truncate" href="#submenu1" data-toggle="collapse"
+              data-target="#submenu1"><i class=""></i> <span class="d-none d-sm-inline">General</span></a>
+            <div class="collapse" id="submenu1" aria-expanded="false">
+              <ul class="flex-column pl-2 nav">
+                <li class="nav-item"><a class="nav-link py-0" href="#"><span>subclass1</span></a></li>
+                <li class="nav-item"><a class="nav-link py-0" href="#"><span>subclass2</span></a></li>
+                <li class="nav-item"><a class="nav-link py-0" href="#"><span>subclass3</span></a></li>
+              </ul>
+            </div>
+            <a class="nav-link collapsed text-truncate" href="#submenu2" data-toggle="collapse"
+              data-target="#submenu2"><i class=""></i> <span class="d-none d-sm-inline">History</span></a>
+            <div class="collapse" id="submenu2" aria-expanded="false">
+              <ul class="flex-column pl-2 nav">
+                <li class="nav-item"><a class="nav-link py-0" href="#"><span>subclass1</span></a></li>
+                <li class="nav-item"><a class="nav-link py-0" href="#"><span>subclass2</span></a></li>
+                <li class="nav-item"><a class="nav-link py-0" href="#"><span>subclass3</span></a></li>
+              </ul>
+            </div>
+            <a class="nav-link collapsed text-truncate" href="#submenu3" data-toggle="collapse"
+              data-target="#submenu3"><i class=""></i> <span class="d-none d-sm-inline">Storage</span></a>
+            <div class="collapse" id="submenu3" aria-expanded="false">
+              <ul class="flex-column pl-2 nav">
+                <li class="nav-item"><a class="nav-link py-0" href="#"><span>subclass1</span></a></li>
+                <li class="nav-item"><a class="nav-link py-0" href="#"><span>subclass2</span></a></li>
+                <li class="nav-item"><a class="nav-link py-0" href="#"><span>subclass3</span></a></li>
+              </ul>
+            </div>
+          </li>
+
+        </ul>
+      </div>
+
+      <div class="text-center mt-5">
+        <button class="btn btn-danger" style="width: 100%;">Impeachment poll</button>
+        <button class="btn btn-danger mt-2" style="width: 100%;">Request for revolution</button>
+
+      </div>
+
+    </div>
+
+    <div class="col-lg-9 ">
+      <div class="row">
+        <div class="col-lg-3">
+          <button class="btn btn-danger" style="width: 120%;">Impeachment Poll</button>
         </div>
-<div class="row">
-
-  <!-- Post Content Column -->
-  <div class="col-md-3">
- <button class="btn btn-primary mt-3">Join in</button>
- <!-- Side Widget -->
- <div class="card my-4">
-      <h5 class="card-header">General</h5>
-      <div class="card-body">
-        <div class="row">
-          <div class="col-lg-6">
-            <ul class="list-unstyled mb-0">
-              <li>
-                <a href="#">subclass1</a>
-              </li>
-              <li>
-                <a href="#">subclass2</a>
-              </li>
-              <li>
-                <a href="#">subclass3</a>
-              </li>
-            </ul>
+        <div class="col-lg-8 ml-3">
+          <div class="progress mb-2">
+            <div class="progress-bar bg-danger" role="progressbar" style="width: 51.9%;" aria-valuenow="25"
+              aria-valuemin="0" aria-valuemax="100">51.9%</div>
           </div>
-         
+          <div class="progress">
+            <div class="progress-bar" role="progressbar" style="width: 48.1%;" aria-valuenow="25" aria-valuemin="0"
+              aria-valuemax="100">48.1%</div>
+          </div>
         </div>
       </div>
-    </div>
-    
-    <div class="card my-4">
-      <h5 class="card-header">History</h5>
-      <div class="card-body">
-        <div class="row">
-          <div class="col-lg-6">
-            <ul class="list-unstyled mb-0">
-              <li>
-                <a href="#">subclass1</a>
-              </li>
-              <li>
-                <a href="#">subclass2</a>
-              </li>
-              <li>
-                <a href="#">subclass3</a>
-              </li>
-            </ul>
+      <div class="row">
+        <div class="col-lg-6 mt-3">
+
+          <div class="row align-items-center">
+            <div class="col-6">
+              <h4>Hot</h4>
+            </div>
+            <div class="col-6 mb-2">
+              <button class="btn btn-xs"><span class="fa fa-book"></span></button>
+              <button class="btn btn-xs"><span class="fa fa-list"></span></button>
+              <button class="btn btn-xs"><span class="fa fa-info"></span> More</button>
+            </div>
           </div>
-         
+
+          <table class="table table-bordered table-sm">
+            <thead>
+              <tr>
+                <th>[subclass]</th>
+                <th>Title</th>
+                <th>Details</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>John</td>
+                <td>Doe</td>
+                <td>john@example.com</td>
+              </tr>
+              <tr>
+                <td>Mary</td>
+                <td>Moe</td>
+                <td>mary@example.com</td>
+              </tr>
+              <tr>
+                <td>July</td>
+                <td>Dooley</td>
+                <td>july@example.com</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
+
+        <div class="col-lg-6 mt-3">
+        <div class="row align-items-center">
+            <div class="col-6">
+              <h4>General</h4>
+            </div>
+            <div class="col-6 mb-2">
+              <button class="btn btn-xs"><span class="fa fa-book"></span></button>
+              <button class="btn btn-xs"><span class="fa fa-list"></span></button>
+              <button class="btn btn-xs"><span class="fa fa-info"></span> More</button>
+            </div>
+          </div>
+          <table class="table table-bordered table-sm">
+            <thead>
+              <tr>
+                <th>[subclass]</th>
+                <th>Title</th>
+                <th>Details</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>John</td>
+                <td>Doe</td>
+                <td>john@example.com</td>
+              </tr>
+              <tr>
+                <td>Mary</td>
+                <td>Moe</td>
+                <td>mary@example.com</td>
+              </tr>
+              <tr>
+                <td>July</td>
+                <td>Dooley</td>
+                <td>july@example.com</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+
+      </div>
+
+      <div class="row">
+        <div class="col-lg-6 mt-3">
+        <div class="row align-items-center">
+            <div class="col-6">
+              <h4>History</h4>
+            </div>
+            <div class="col-6 mb-2">
+              <button class="btn btn-xs"><span class="fa fa-book"></span></button>
+              <button class="btn btn-xs"><span class="fa fa-list"></span></button>
+              <button class="btn btn-xs"><span class="fa fa-info"></span> More</button>
+            </div>
+          </div>
+          <table class="table table-bordered table-sm">
+            <thead>
+              <tr>
+                <th>[subclass]</th>
+                <th>Title</th>
+                <th>Details</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>John</td>
+                <td>Doe</td>
+                <td>john@example.com</td>
+              </tr>
+              <tr>
+                <td>Mary</td>
+                <td>Moe</td>
+                <td>mary@example.com</td>
+              </tr>
+              <tr>
+                <td>July</td>
+                <td>Dooley</td>
+                <td>july@example.com</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+        <div class="col-lg-6 mt-3">
+        <div class="row align-items-center">
+            <div class="col-6">
+              <h4>Travel</h4>
+            </div>
+            <div class="col-6 mb-2">
+              <button class="btn btn-xs"><span class="fa fa-book"></span></button>
+              <button class="btn btn-xs"><span class="fa fa-list"></span></button>
+              <button class="btn btn-xs"><span class="fa fa-info"></span> More</button>
+            </div>
+          </div>
+
+          <table class="table table-bordered table-sm">
+            <thead>
+              <tr>
+                <th>[subclass]</th>
+                <th>Title</th>
+                <th>Details</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>John</td>
+                <td>Doe</td>
+                <td>john@example.com</td>
+              </tr>
+              <tr>
+                <td>Mary</td>
+                <td>Moe</td>
+                <td>mary@example.com</td>
+              </tr>
+              <tr>
+                <td>July</td>
+                <td>Dooley</td>
+                <td>july@example.com</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
       </div>
     </div>
-    
-    <div class="card my-4">
-      <h5 class="card-header">Storage   </h5>
-      <div class="card-body">
-        <div class="row">
-          <div class="col-lg-6">
-            <ul class="list-unstyled mb-0">
-              <li>
-                <a href="#">subclass1</a>
-              </li>
-              <li>
-                <a href="#">subclass2</a>
-              </li>
-              <li>
-                <a href="#">subclass3</a>
-              </li>
-            </ul>
-          </div>
-         
-        </div>
-      </div>
-    </div>
-    
-
-</div>
-
-  <div class="col-lg-9 mt-4">
-    
-    <div class="text-center">
-    <button class="btn btn-danger mb-2">Impeachment poll</button>
-    <div class="progress">
-        <div class="progress-bar" style="width:70%"></div>
-    </div>
-    </div>
-
-    <div class="row">
-    <div class="col-lg-6 mt-3">
-    <h4>Hot</h4>
-    <table class="table table-bordered table-sm">
-    <thead>
-      <tr>
-        <th>Firstname</th>
-        <th>Lastname</th>
-        <th>Email</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>John</td>
-        <td>Doe</td>
-        <td>john@example.com</td>
-      </tr>
-      <tr>
-        <td>Mary</td>
-        <td>Moe</td>
-        <td>mary@example.com</td>
-      </tr>
-      <tr>
-        <td>July</td>
-        <td>Dooley</td>
-        <td>july@example.com</td>
-      </tr>
-    </tbody>
-  </table>
-    </div>
-
-    <div class="col-lg-6 mt-3">
-    <h4>General</h4>
-    <table class="table table-bordered table-sm">
-    <thead>
-      <tr>
-        <th>Firstname</th>
-        <th>Lastname</th>
-        <th>Email</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>John</td>
-        <td>Doe</td>
-        <td>john@example.com</td>
-      </tr>
-      <tr>
-        <td>Mary</td>
-        <td>Moe</td>
-        <td>mary@example.com</td>
-      </tr>
-      <tr>
-        <td>July</td>
-        <td>Dooley</td>
-        <td>july@example.com</td>
-      </tr>
-    </tbody>
-  </table>
-    </div>
 
 
-    </div>
 
-    <div class="row">
-    <div class="col-lg-6 mt-3">
-    <h4>History</h4>
-    <table class="table table-bordered table-sm">
-    <thead>
-      <tr>
-        <th>Firstname</th>
-        <th>Lastname</th>
-        <th>Email</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>John</td>
-        <td>Doe</td>
-        <td>john@example.com</td>
-      </tr>
-      <tr>
-        <td>Mary</td>
-        <td>Moe</td>
-        <td>mary@example.com</td>
-      </tr>
-      <tr>
-        <td>July</td>
-        <td>Dooley</td>
-        <td>july@example.com</td>
-      </tr>
-    </tbody>
-  </table>
-    </div>
 
-    <div class="col-lg-6 mt-3">
-    <h4>Travel</h4>
-    <table class="table table-bordered table-sm">
-    <thead>
-      <tr>
-        <th>Firstname</th>
-        <th>Lastname</th>
-        <th>Email</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>John</td>
-        <td>Doe</td>
-        <td>john@example.com</td>
-      </tr>
-      <tr>
-        <td>Mary</td>
-        <td>Moe</td>
-        <td>mary@example.com</td>
-      </tr>
-      <tr>
-        <td>July</td>
-        <td>Dooley</td>
-        <td>july@example.com</td>
-      </tr>
-    </tbody>
-  </table>
-    </div>
 
-    </div>
-</div>
-  
-   
-
- 
-
-</div>
-<!-- /.row -->
+  </div>
+  <!-- /.row -->
 
 </div>
