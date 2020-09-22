0530123de0d7c1afb114d3dc08390e197a237567
weendigo <weendilife@gmail.com>
2020-09-23 00:01:52 +0900

    env setup

--- app/Config/App.php
+++ app/Config/App.php
@@ -21,7 +21,7 @@ class App extends BaseConfig
 	| environments.
 	|
 	*/
-	public $baseURL = 'http://localhost/weendi/';
+	public $baseURL = 'http://nodaq.com/';
 
 	/*
 	|--------------------------------------------------------------------------
