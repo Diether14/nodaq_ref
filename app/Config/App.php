3b767dd32ba2c9a0431d855347982a3dea1d13dc
root <root@nodaq-tokyo.asia-northeast1-b.c.nodaq-290408.internal>
2020-09-29 17:44:59 +0000

    fixed some compatability issues

--- app/Config/App.php
+++ app/Config/App.php
@@ -21,7 +21,7 @@ class App extends BaseConfig
 	| environments.
 	|
 	*/
-	public $baseURL = 'http://localhost/weendi/';
+	public $baseURL = 'http://34.84.190.89/';
 
 	/*
 	|--------------------------------------------------------------------------
