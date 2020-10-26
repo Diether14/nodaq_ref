c535755f9f472a48ed740767dbc2ce8f462ce986
Diether <llenaresdiether@gmail.com>
2020-10-26 21:28:58 +0800

    cookie consent

--- app/Config/App.php
+++ app/Config/App.php
@@ -21,7 +21,7 @@ class App extends BaseConfig
 	| environments.
 	|
 	*/
-	public $baseURL = 'http://nodaq.com';
+	public $baseURL = 'http://dev.nodaq.com';
 	/*
 	|--------------------------------------------------------------------------
 	| Index File
--- app/Config/Database.php
+++ app/Config/Database.php
@@ -31,10 +31,10 @@ class Database extends \CodeIgniter\Database\Config
 	 */
 	public $default = [
 		'DSN'      => '',
-		'hostname' => '10.110.224.3',
-		'username' => 'nodaq',
-		'password' => 'q38Gb74&21@pP%',
-		'database' => 'nodaq_asia',
+		'hostname' => 'localhost',
+		'username' => 'root',
+		'password' => '',
+		'database' => 'weendi',
 		'DBDriver' => 'MySQLi',
 		'DBPrefix' => '',
 		'pConnect' => false,
